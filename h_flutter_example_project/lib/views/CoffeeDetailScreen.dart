272d0bf14753b5438f06ec12febaa1d7ba65f41b
jinnyjinny12 <154886745+jinnyjinny12@users.noreply.github.com>
2024-08-28 22:40:06 +0900

    Update CoffeeDetailScreen.dart

--- h_flutter_example_project/lib/views/CoffeeDetailScreen.dart
+++ h_flutter_example_project/lib/views/CoffeeDetailScreen.dart
@@ -1,27 +1,23 @@
-
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:h_flutter_example_project/models/CoffeeItem.dart';
 
-class CoffeeDetialscreen extends StatelessWidget {
+class CoffeeDetailScreen extends StatelessWidget{
+  CoffeeItem? coffeeItem;
 
-  final CoffeeItem coffeeItem;
-
-  CoffeeDetialscreen({required this.coffeeItem, super.key});
-
+  CoffeeDetailScreen({required this.coffeeItem, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(coffeeItem.title.toString()),
+      appBar: AppBar(title: Text(coffeeItem!.title.toString())),
+      body: Column(
+        children: [
+          Image.network(coffeeItem!.image.toString()),
+          const SizedBox(height: 16),
+          Text(coffeeItem!.description.toString())
+        ],
       ),
-      body:  Center(
-        child: Text(''),
-      ),
-    )
-
+    );
   }
 }
